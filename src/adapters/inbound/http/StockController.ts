dce0e3b53de5e59d89c629032f38734d6a6b912b
ymbossio <yovanis2001@gmail.com>
2025-05-25 14:52:48 -0500

    fix services stock and get all products

--- src/adapters/inbound/http/StockController.ts
+++ src/adapters/inbound/http/StockController.ts
@@ -6,18 +6,18 @@ import { UpdateStock } from "../../../domains/useCases/UpdateStock";
 export class StockController {
   constructor(private updateStockUseCase: UpdateStock) {}
 
-  async updateStock(req: Request, res: Response): Promise<void> {
+  async updateStock(req: Request, res: Response): Promise<void> {    
+
     try {
-      const { id_products, available } = req.body;
+      const { id_products, available } = req.body;      
 
       if (typeof id_products !== "number" || typeof available !== "number") {
         res.status(400).json({ message: "Invalid input types" });
         return;
-      }
-
+      } 
       await this.updateStockUseCase.execute(id_products, available);
 
-      res.status(200).json({ message: "Stock updated successfully" });
+      res.status(200).json({ message: "Stock updated successfully"});
     } catch (error: any) {
       console.error("Error updating stock:", error);
       res.status(500).json({ message: "Error updating stock", error: error.message });
--- src/adapters/outbound/db/SequelizeProductRepository.ts
+++ src/adapters/outbound/db/SequelizeProductRepository.ts
@@ -2,12 +2,20 @@ import { ProductRepository } from "../../../ports/ProductRepository";
 import { Products } from "../../../domains/entities/Products";
 import { ProductModel } from "../models/ProductModel";
 import { StockModel } from "../models/StockModel";
+import { Op } from "sequelize";
 
 export class SequelizeProductRepository implements ProductRepository {
  
   async findAll(): Promise<Products[]> {
   const products = await ProductModel.findAll({
-    include: [{ model: StockModel }]
+    include: [{ 
+      model: StockModel,
+      where: {
+        available: {
+          [Op.gt]: 0
+        }
+      } 
+    }]
   });
 
   return products.map((p) => new Products(
